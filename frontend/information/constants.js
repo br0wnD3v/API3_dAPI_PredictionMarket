98164a9c82a42c2c42bbbfd2d2abe580ba7267b4
br0wnD3v <bothereum@protonmail.com>
2023-07-10 21:48:44 +0530

    Minor Updates.

---
+++ frontend/information/constants.js
@@ -0,0 +1,740 @@
+export const assetToProxy = {
+  USD: "0x26690f9f17fdc26d419371315bc17950a0fc90ed",
+  AAVE: "0x13d1Ed8c24911d88e6155cE32A66908399C97924",
+  API3: "0xf25B7429406B24dA879F0D1a008596b74Fcb9C2F",
+};
+
+export const usdcAddress = "0x31a0EDA48F467105EF31909465a7403d2e657bCC";
+export const usdcABI = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    stateMutability: "payable",
+    type: "fallback",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+    ],
+    name: "allowance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "subtractedValue",
+        type: "uint256",
+      },
+    ],
+    name: "decreaseAllowance",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "addedValue",
+        type: "uint256",
+      },
+    ],
+    name: "increaseAllowance",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "receiver",
+        type: "address",
+      },
+    ],
+    name: "mint",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "transferFrom",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+];
+
+export const tradingAddress = "0x027bE57490473534aDbb399c39eDBc91310fE99E";
+export const tradingABI = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_usdc",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "PM_InsufficientApprovedAmount",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "predictionId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "timestamp",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "bool",
+        name: "isAbove",
+        type: "bool",
+      },
+      {
+        indexed: false,
+        internalType: "int224",
+        name: "priceReading",
+        type: "int224",
+      },
+      {
+        indexed: false,
+        internalType: "int224",
+        name: "priceTarget",
+        type: "int224",
+      },
+    ],
+    name: "ConcludeFatalError",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "predictionId",
+        type: "uint256",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "marketHandler",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "trader",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "int256",
+        name: "amountYes",
+        type: "int256",
+      },
+      {
+        indexed: false,
+        internalType: "int256",
+        name: "amountNo",
+        type: "int256",
+      },
+    ],
+    name: "HandlerProgress",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "predictionId",
+        type: "uint256",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "marketHandler",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "creator",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "timestamp",
+        type: "uint256",
+      },
+    ],
+    name: "PredictionCreated",
+    type: "event",
+  },
+  {
+    stateMutability: "payable",
+    type: "fallback",
+  },
+  {
+    inputs: [],
+    name: "PLATFORM_FEE",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_predictionId",
+        type: "uint256",
+      },
+      {
+        internalType: "bool",
+        name: "_vote",
+        type: "bool",
+      },
+    ],
+    name: "concludePrediction_2",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_tokenSymbol",
+        type: "string",
+      },
+      {
+        internalType: "address",
+        name: "_proxyAddress",
+        type: "address",
+      },
+      {
+        internalType: "bool",
+        name: "_isAbove",
+        type: "bool",
+      },
+      {
+        internalType: "int224",
+        name: "_targetPricePoint",
+        type: "int224",
+      },
+      {
+        internalType: "uint256",
+        name: "_fee",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_deadline",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_basePrice",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "_caller",
+        type: "address",
+      },
+    ],
+    name: "createPrediction",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_predictionId",
+        type: "uint256",
+      },
+    ],
+    name: "getPrediction",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "string",
+            name: "tokenSymbol",
+            type: "string",
+          },
+          {
+            internalType: "int224",
+            name: "targetPricePoint",
+            type: "int224",
+          },
+          {
+            internalType: "bool",
+            name: "isAbove",
+            type: "bool",
+          },
+          {
+            internalType: "address",
+            name: "proxyAddress",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "fee",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "timestamp",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "deadline",
+            type: "uint256",
+          },
+          {
+            internalType: "bool",
+            name: "isActive",
+            type: "bool",
+          },
+          {
+            internalType: "address",
+            name: "marketHandler",
+            type: "address",
+          },
+        ],
+        internalType: "struct Prediction",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_predictionId",
+        type: "uint256",
+      },
+    ],
+    name: "getProxyAddressForPrediction",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_settlement",
+        type: "address",
+      },
+    ],
+    name: "setSettlementAddress",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_vault",
+        type: "address",
+      },
+    ],
+    name: "setVaultAddress",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "settlementAddress",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "_caller",
+        type: "address",
+      },
+      {
+        internalType: "int256",
+        name: "_amountYes",
+        type: "int256",
+      },
+      {
+        internalType: "int256",
+        name: "_amountNo",
+        type: "int256",
+      },
+    ],
+    name: "trackProgress",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "vaultAddress",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+];
--- frontend/pages/components/Home/Hero/Create.js
+++ frontend/pages/components/Home/Hero/Create.js
@@ -1,3 +1,131 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Box,
+  FormErrorMessage,
+  Button,
+} from "@chakra-ui/react";
+
+import { usdcAddress, usdcABI } from "@/information/constants";
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useContractRead,
+  useAccount,
+} from "wagmi";
+import { tradingAddress } from "@/information/constants";
+
+import { useEffect, useState } from "react";
+
 export default function Create() {
-  return <>Create</>;
+  const { address } = useAccount();
+
+  const [error, setError] = useState("");
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [startOperation, setStartOperation] = useState(false);
+  const [approved, setApproved] = useState(false);
+
+  const [tokenType, setTokenType] = useState("ETH");
+  const [dueDate, setDueDate] = useState(null);
+  const [targetPrice, setTargetPrice] = useState(null);
+  const [isAbove, setIsAbove] = useState(false);
+
+  const { config, error: contractWriteError } = usePrepareContractWrite({
+    address: usdcAddress,
+    abi: usdcABI,
+    functionName: "approve",
+    args: [tradingAddress, 50000000n],
+  });
+
+  // Get the write function
+  const {
+    data: writeData,
+    isLoading: writeLoading,
+    write,
+  } = useContractWrite(config);
+
+  useEffect(() => {
+    if (startOperation) {
+      write();
+    }
+  }, [startOperation]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setIsSubmitting(true);
+    // validate form data
+    if (!dueDate || !tokenType || !targetPrice || !isAbove) {
+      setError("Please fill out all required fields.");
+      setIsSubmitting(false);
+      return;
+    }
+    const dueDateObject = new Date(dueDate);
+    const currentDateObject = new Date();
+    if (dueDateObject <= currentDateObject) {
+      setError("Due date must be in the future.");
+      setIsSubmitting(false);
+      return;
+    }
+    const unixEpochTime = Math.floor(dueDateObject.getTime() / 1000).toString();
+    setDueDate(unixEpochTime);
+
+    setError("");
+
+    setStartOperation(true);
+  };
+
+  return (
+    <>
+      <Box align="center" justify="center" pt={5}>
+        <Box w="50%" border="2px solid black" borderRadius={10} p={10}>
+          <form onSubmit={handleSubmit}>
+            <FormControl isRequired isInvalid={error}>
+              <FormLabel>Asset To Predict</FormLabel>
+              <Select
+                disabled={isSubmitting}
+                value={tokenType}
+                onChange={(e) => setTokenType(e.target.value)}
+              >
+                <option value="ETH">ETH</option>
+                <option value="AAVE">AAVE</option>
+                <option value="API3">API3</option>
+              </Select>
+              <FormLabel mt={5}>Deadline</FormLabel>
+              <Input
+                disabled={isSubmitting}
+                type="date"
+                value={dueDate}
+                onChange={(e) => setDueDate(e.target.value)}
+              />
+              <FormLabel mt={5}>Target Price In USD</FormLabel>
+              <Input
+                placeholder="1000"
+                type="number"
+                disabled={isSubmitting}
+                value={targetPrice}
+                onChange={(e) => setTargetPrice(e.target.value)}
+              />
+              <FormLabel mt={5}>Will Be Above The Target?</FormLabel>
+              <Select
+                disabled={isSubmitting}
+                value={isAbove}
+                onChange={(e) => setIsAbove(e.target.value)}
+              >
+                <option value="true">True</option>
+                <option value="false">False</option>
+              </Select>
+              <Button colorScheme="green" onClick={handleSubmit} mt={5}>
+                Create
+              </Button>
+              <FormErrorMessage>{error}</FormErrorMessage>
+            </FormControl>
+          </form>
+        </Box>
+      </Box>
+    </>
+  );
 }
