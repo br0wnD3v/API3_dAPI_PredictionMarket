076bfca2b002daff9f6d1f9e340dbf46415aff27
br0wnD3v <bothereum@protonmail.com>
2023-07-10 10:12:08 +0530

    Minor Updates.

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,27 @@
 import HomeLayout from "./components/Home/HomeLayout";
 
+import { useState, useEffect } from "react";
+
+const FontWrapper = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    // Wait for the font to load before showing the text
+    document.fonts.ready.then(() => setIsVisible(true));
+  }, []);
+
+  return (
+    <div style={{ visibility: isVisible ? "visible" : "hidden" }}>
+      {children}
+    </div>
+  );
+};
 export default function Home() {
   return (
     <>
-      <HomeLayout />
+      <FontWrapper>
+        <HomeLayout />
+      </FontWrapper>
     </>
   );
 }
