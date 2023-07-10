bbe41d3a72ab4de90e89bf841fedab8e10b90a91
br0wnD3v <bothereum@protonmail.com>
2023-07-10 10:06:57 +0530

    Minor Updates.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   env: {
     WALLET_CLOUD_PROJECT_ID: "b18a057500f1ca54b904df37574c961b",
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
