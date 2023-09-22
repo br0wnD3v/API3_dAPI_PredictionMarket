aa916ca53d72ab957ce82e78cc5a222ca8b9058f
br0wnD3v <bothereum@protonmail.com>
2023-09-22 16:05:47 +0530

    Jesus.

--- graph/node_modules/@graphprotocol/graph-cli/dist/protocols/ipfs/codegen/file_template.d.ts
+++
@@ -1,9 +0,0 @@
-import immutable from 'immutable';
-import * as tsCodegen from '../../../codegen/typescript';
-export default class IpfsFileTemplateCodeGen {
-    private template;
-    constructor(template: immutable.Map<any, any>);
-    generateModuleImports(): never[];
-    generateCreateMethod(): tsCodegen.StaticMethod;
-    generateCreateWithContextMethod(): tsCodegen.StaticMethod;
-}
